9464a65bffe0edb1211595c8e902f54e6838586b
strongwealth <strongwealthslimzy@gmail.com>
2020-10-02 00:54:28 +0100

    loaded jquery via https

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="css/bootstrap.min.css" />
   <link rel="stylesheet" href="css/main.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no">
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
   </script>
   <script src="js/bootstrap.min.js"></script>
 </head>
