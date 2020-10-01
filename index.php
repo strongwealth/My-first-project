2777e4bfdabd201cc18c239697a566c78a2d7309
strongwealth <strongwealthslimzy@gmail.com>
2020-10-02 00:08:00 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Red Stone Shop</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+  </script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="/tutorial/index.php" class="navbar-brand">Red Stone Shop</a>
+    <ul class="nav navbar-nav">
+      <!--Drop down menu-->
+      <li class="dropdown">
+        <console.log=$0 a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Men<span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Shirts</a></li>
+          <li><a href="#">Pants</a></li>
+          <li><a href="#">Shoes</a></li>
+          <li><a href="#">Accessories</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
