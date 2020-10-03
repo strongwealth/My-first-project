09e6732fab94ce87b3f9b952aaa86ebb8df2386c
strongwealth <strongwealthslimzy@gmail.com>
2020-10-03 21:49:45 +0100

    update css.main

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
   <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top" id="navbar">
   <div class="container">
-    <a href="/tutorial/index.php" class="navbar-brand">Red Stone Shop</a>
+    <a href="/tutorial/index.php" class="navbar-brand" id="text">Red Stone Shop</a>
     <ul class="nav navbar-nav">
       <!--Drop down menu-->
       <li class="dropdown">
@@ -27,5 +27,92 @@
     </ul>
   </div>
 </nav>
-</body>
+<!-- Creating Images-->
+<div id="background-image">
+  <div id="image-1">
+    <div id="image-2">
+    </div>
+  </div>
+</div>
+
+
+<!--Removing the spaces and center the products-->
+
+<div class="" style="display:flex; justify-content:center">
+
+<!-- main content of featured products-->
+  <div class="col-md-8">
+
+    <h2 class="text-center">Featured Products</h2>
+    <div class="row">
+    <div class="col-md-3">
+      <h4>Levis jeans</h4>
+      <img src="images/trouser.jpg" alt="Levis jeans" id="images"/>
+      <p class="list-price text-danger">List price: <s>$24.99</s></p>
+      <p class="price">Our price: $19.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Addidas Football</h4>
+      <img src="images/addidas.jpg" alt="Addidas Football" id="images"/>
+      <p class="list-price text-danger">List price: <s>$39.99</s></p>
+      <p class="price">Our price: $29.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Watch</h4>
+      <img src="images/rolex.jpg" alt="Watch" id="images"/>
+      <p class="list-price text-danger">List price: <s>$20.99</s></p>
+      <p class="price">Our price: $17.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-3">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Polo Shirt</h4>
+      <img src="images/polo.jpg" alt="Polo Shirt" id="images"/>
+      <p class="list-price text-danger">List price: <s>$54.99</s></p>
+      <p class="price">Our price: $19.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-4">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Head Band</h4>
+      <img src="images/band.jpg" alt="Head Band" id="images"/>
+      <p class="list-price text-danger">List price: <s>$20.99</s></p>
+      <p class="price">Our price: $15.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-5">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Shoes</h4>
+      <img src="images/jordan.jpg" alt="Shoes" id="images"/>
+      <p class="list-price text-danger">List price: <s>$24.99</s></p>
+      <p class="price">Our price: $19.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-6">Details</button>
+    </div>
+
+    <div class="col-md-3">
+      <h4>Tennis ball</h4>
+      <img src="images/tennis.jpg" alt="Tennis ball" id="images"/>
+      <p class="list-price text-danger">List price: <s>$50.99</s></p>
+      <p class="price">Our price: $15.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-7">Details</button>
+    </div>
+
+
+    <div class="col-md-3">
+      <h4>Bag</h4>
+      <img src="images/bag.jpg" alt="Bag" id="images"/>
+      <p class="list-price text-danger">List price: <s>$30.99</s></p>
+      <p class="price">Our price: $20.99</p>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-8">Details</button>
+    </div>
+  </div>
+</div>
+</div>
+  <footer class="text-center" id="footer">&copy; Copyright 2019-2020 Red Stone shop</footer>
+  </body>
+
 </html>
